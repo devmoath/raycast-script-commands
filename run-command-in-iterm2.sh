3a3c65d21f6aea04fe05d72cd724a0628c986be3
Moath <moath.alhajrii@gmail.com>
2022-03-26 19:15:00 +0300

    add `Run command in iTerm2` script command

---
+++ run-command-in-iterm2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Run command in iTerm2
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon icons/iTerm.png
+# @raycast.argument1 { "type": "text", "placeholder": "command" }
+
+# Documentation:
+# @raycast.author Moath Alhajri
+# @raycast.authorURL https://github.com/DevMoath
+
+if ! command -v ttab &>/dev/null; then
+    echo 'Run Command in iTerm2 require ttab package. `npm install ttab -g` to install it'
+    exit 1
+fi
+
+ttab -a iTerm2 -d '' "$1"
+
+echo 'Your command has been ran'
